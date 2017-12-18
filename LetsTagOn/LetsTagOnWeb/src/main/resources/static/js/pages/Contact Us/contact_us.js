e6461fd48fac542f37d9b53e60f924e82ceb4e13
Sathya Udhayakumar <Sathya Udhayakumar@LAPTOP-83F7JAJ0>
2017-12-18 12:58:03 +0530

    Revert "Issue 27 - Full front end and back end changes"
    This reverts commit fd1d473523d8276d35fbe21716de22b0f6614189.

--- LetsTagOn/LetsTagOnWeb/src/main/resources/static/js/pages/Contact Us/contact_us.js
+++ LetsTagOn/LetsTagOnWeb/src/main/resources/static/js/pages/Contact Us/contact_us.js
@@ -2,36 +2,8 @@ var contactUsModule = angular.module('contact us',[]);
 /**
 	contact us controller
  **/
-contactUsModule.controller('contactuscontroller',function($http, $scope, $rootScope, $location, $timeout){
-	$scope.customer = {};
-	
+contactUsModule.controller('contactuscontroller',function($scope,$location){
 	$scope.getContactUsPage = function(){
 		$location.path('/contact us')
 	}
-	$scope.contactUs = function(submitted) {
-		$scope.customer.userName =	$scope.user.name;
-		$scope.customer.emailAddress =	$scope.user.email;
-		$scope.customer.summary =	$scope.user.message;
-		$('#loading-indicator').show();
-		
-        $http({
-            url: 'contact/us',
-            dataType: 'json',
-            method: 'POST',
-            data: $scope.customer,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).success(function(response) {
-        	$('#loading-indicator').hide();
-            if (response.error == null) {
-            	$scope.user = '';
-            	$('#successmessage').css({
-                    display: "block",
-                });
-            }
-        }).error(function(error) {
-            console.log("error while getting notification list for the userID" + $rootScope.userId);
-        });
-    }
 });
