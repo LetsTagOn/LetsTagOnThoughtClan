5f45d4ffdeb55d0e5a8b5897e4173b0c3ea2a7a0
Vinod Kumar <vkviswa@gmail.com>
2018-01-04 19:17:48 +0530

    issue#23 - improve UX for linking to about us page and add more styling:
    When user clicked on about us link in footer the about us page loaded scrolled to the bottom

--- LetsTagOn/LetsTagOnWeb/src/main/resources/static/js/pages/About Us/about_us.js
+++ LetsTagOn/LetsTagOnWeb/src/main/resources/static/js/pages/About Us/about_us.js
@@ -1,14 +1,17 @@
-
-var aboutUsModule = angular.module('about us',[]);
+var aboutUsModule = angular.module("about us", []);
 
 /**
  * Controller for About Us Module
  * 
  **/
 
-aboutUsModule.controller('aboutcontroller',
-		function($scope,$location){
-	$scope.getAboutUsPage = function(){
-		$location.path('/about us');
-	}
+aboutUsModule.controller("aboutcontroller", function(
+	$scope,
+	$location,
+	$anchorScroll
+) {
+	$scope.getAboutUsPage = function() {
+		$location.path("/about us");
+		$anchorScroll("top");
+	};
 });
