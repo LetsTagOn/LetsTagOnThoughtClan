77fa7230bd60bf0a49fe1c1fa58cb49ffc5f30d3
Vinod Kumar <vkviswa@gmail.com>
2018-01-24 11:05:32 +0530

    miscellaneous list of changes: add new user text before register in login modal restyle login modal so that new user? register text appears in one line expand bus. in sravanis title in about us page do not redirect to home page if user logs in while applying for event set carousel speed to 2500ms correct layout in opp detail page so that the right sidebar appears correctly

--- LetsTagOn/LetsTagOnWeb/src/main/resources/static/js/pages/common/main.js
+++ LetsTagOn/LetsTagOnWeb/src/main/resources/static/js/pages/common/main.js
@@ -3,102 +3,126 @@
  * Main application controller MainController
  * 
  */
-var letsTagOn = angular.module('main', ['ui.bootstrap', 'ngMessages', 'bootstrapModal', 'completeProfile']);
-letsTagOn.controller('MainController', function($rootScope, $http, $location, $route, $scope, $timeout) {
-
+var letsTagOn = angular.module("main", [
+    "ui.bootstrap",
+    "ngMessages",
+    "bootstrapModal",
+    "completeProfile"
+]);
+letsTagOn.controller("MainController", function(
+    $rootScope,
+    $http,
+    $location,
+    $route,
+    $scope,
+    $timeout
+) {
     //TO get baseUrl
     var url = $location.absUrl();
     var arr = url.split("#");
     $rootScope.baseUrl = arr[0];
 
-    $rootScope.unrestrictedUrls = ["/resetPassword", "/search", ];
+    $rootScope.unrestrictedUrls = ["/resetPassword", "/search"];
 
     var authenticate = function(callback) {
-        $http.get('user').success(function(data) {
-            if (data.name) {
-                $rootScope.authenticated = true;
-                $('#modalLogin').modal('hide');
-                $('body').removeClass('modal-open');
-                $('.modal-backdrop').remove();
-                $rootScope.authenticated = true;
-                sessionStorage.authenticated = true;
-                $rootScope.logout = true;
-                $scope.error = false;
-                $rootScope.userId = data.principal.id;
-                $rootScope.userObj = data.principal.user;
-                $rootScope.profilePicture = data.principal.user.profilePicture;
-            } else {
+        $http
+            .get("user")
+            .success(function(data) {
+                if (data.name) {
+                    $rootScope.authenticated = true;
+                    $("#modalLogin").modal("hide");
+                    $("body").removeClass("modal-open");
+                    $(".modal-backdrop").remove();
+                    $rootScope.authenticated = true;
+                    sessionStorage.authenticated = true;
+                    $rootScope.logout = true;
+                    $scope.error = false;
+                    $rootScope.userId = data.principal.id;
+                    $rootScope.userObj = data.principal.user;
+                    $rootScope.profilePicture =
+                        data.principal.user.profilePicture;
+                } else {
+                    $rootScope.authenticated = false;
+                    sessionStorage.authenticated = false;
+                    //check if url allowed though not authenticated
+
+                    var allowed = false;
+                    for (i in $rootScope.unrestrictedUrls) {
+                        var url = $rootScope.unrestrictedUrls[i];
+                        if ($location.absUrl().indexOf(url) >= 0) {
+                            allowed = true;
+                            break;
+                        }
+                    }
+
+                    if (!allowed) $location.path("/welcome");
+                }
+                callback && callback();
+            })
+            .error(function() {
                 $rootScope.authenticated = false;
                 sessionStorage.authenticated = false;
-                //check if url allowed though not authenticated
-
-                var allowed = false;
-                for (i in $rootScope.unrestrictedUrls) {
-                    var url = $rootScope.unrestrictedUrls[i];
-                    if ($location.absUrl().indexOf(url) >= 0) {
-                        allowed = true;
-                        break;
-                    }
-                }
-
-                if (!allowed)
-                    $location.path("/welcome");
-            }
-            callback && callback();
-        }).error(function() {
-            $rootScope.authenticated = false;
-            sessionStorage.authenticated = false;
-            callback && callback();
-        });
+                callback && callback();
+            });
     };
     authenticate();
     $scope.credentials = {};
     $scope.login = function() {
-        $http.post('login', $.param($scope.credentials), {
-            headers: {
-                "content-type": "application/x-www-form-urlencoded"
-            }
-        }).success(function(data) {
-            authenticate(function() {
-                if ($rootScope.authenticated) {
-                	$location.path("/"); // Sourabh: added to redirect to home page on successful login
-                    $scope.error = false;
-                    $scope.submitted = false;
-                    $rootScope.showWallHeader = true;
-                } else {
-                 // Sourabh: Fixed issue of rerouting to home page. This was tryng to relocate to welcome page. Hence commented.   $location.path("/");
-                    $scope.error = true;
-                    $scope.authenticationError = "Invalid credentails";
+        $http
+            .post("login", $.param($scope.credentials), {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded"
                 }
+            })
+            .success(function(data) {
+                authenticate(function() {
+                    if ($rootScope.authenticated) {
+                        if ($location.path() === "/welcome")
+                            $location.path("/"); // Sourabh: added to redirect to home page on successful login
+
+                        $scope.error = false;
+                        $scope.submitted = false;
+                        $rootScope.showWallHeader = true;
+                    } else {
+                        // Sourabh: Fixed issue of rerouting to home page. This was tryng to relocate to welcome page. Hence commented.   $location.path("/");
+                        $scope.error = true;
+                        $scope.authenticationError = "Invalid credentails";
+                    }
+                });
+            })
+            .error(function(data) {
+                $location.path("/welcome");
+                $scope.error = true;
+                $rootScope.authenticated = false;
+                sessionStorage.authenticated = false;
             });
-        }).error(function(data) {
-            $location.path("/welcome");
-            $scope.error = true;
-            $rootScope.authenticated = false;
-            sessionStorage.authenticated = false;
-        });
     };
     $scope.logout = function() {
-        $http.post('logout', {}).success(function() {
-            $scope.redirect("/welcome");
-            $route.reload();
-        }).error(function(data) {
-            $scope.redirect("/welcome");
-        });
-
+        $http
+            .post("logout", {})
+            .success(function() {
+                $scope.redirect("/welcome");
+                $route.reload();
+            })
+            .error(function(data) {
+                $scope.redirect("/welcome");
+            });
     };
-
 
     $scope.redirect = function(redirectLocation) {
         $location.path(redirectLocation);
     };
 
     $scope.getProfileImage = function(userImage) {
-        return $rootScope.baseUrl + "userProfile/getProfilePic?key=" + userImage;
+        return (
+            $rootScope.baseUrl + "userProfile/getProfilePic?key=" + userImage
+        );
     };
 
     $scope.getBannerImage = function(bannerImage) {
-        return $rootScope.baseUrl + "opportunity/getBannerImage?key=" + bannerImage;
+        return (
+            $rootScope.baseUrl + "opportunity/getBannerImage?key=" + bannerImage
+        );
     };
 
     $scope.showAllEvents = function() {
@@ -120,21 +144,20 @@ letsTagOn.controller('MainController', function($rootScope, $http, $location, $r
         window.setInterval(function () {
             if (++idleCounter >= IDLE_TIMEOUT) {
                 if($rootScope.authenticated){
-            	 $scope.logout(); 
+                 $scope.logout(); 
                 }
             }
         }, interval);
     }());*/
 });
-letsTagOn.directive('noSpecialChar', function() {
+letsTagOn.directive("noSpecialChar", function() {
     return {
-        require: 'ngModel',
-        restrict: 'A',
+        require: "ngModel",
+        restrict: "A",
         link: function(scope, element, attrs, modelCtrl) {
             modelCtrl.$parsers.push(function(inputValue) {
-                if (inputValue == null)
-                    return ''
-                cleanInputValue = inputValue.replace(/[^a-z0-9 .\-]/gi, '');
+                if (inputValue == null) return "";
+                cleanInputValue = inputValue.replace(/[^a-z0-9 .\-]/gi, "");
                 if (cleanInputValue != inputValue) {
                     modelCtrl.$setViewValue(cleanInputValue);
                     modelCtrl.$render();
@@ -142,5 +165,5 @@ letsTagOn.directive('noSpecialChar', function() {
                 return cleanInputValue;
             });
         }
-    }
+    };
 });
